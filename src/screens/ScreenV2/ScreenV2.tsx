4a49d30639f76fdbccb0b585dd13cd27e5abf571
Shtono <shtonojs@gmail.com>
2024-05-03 14:34:00 +0300

    Updated ScreenV2

--- src/screens/ScreenV2/ScreenV2.tsx
+++ src/screens/ScreenV2/ScreenV2.tsx
@@ -1,16 +1,38 @@
 import { useSensors } from "hooks"
 import { Stack, Typography } from "@mui/material"
+
+const styles = {
+  containerMain: {},
+  containerCategory: {},
+  containerSensorLeft: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingX: 2,
+    color: "wheat",
+  },
+  containerSensorRight: {
+    flexDirection: "row",
+    justifyContent: "end",
+    alignItems: "center",
+    paddingX: 2,
+    color: "wheat",
+    gap: 2,
+    mb: 2,
+  },
+}
 const ScreenV2 = () => {
   const { data: sensors, isLoading } = useSensors()
 
   if (!sensors?.length) return <Typography>Loading...</Typography>
 
-  // console.log("Sensors: ", sensors)
+  console.log("Sensors: ", sensors)
 
   // return <div>123</div>
 
   const gpuTemp = `${sensors[0].value} ${sensors[0].unit}`
   const gpuLoad = `${sensors[2].value} ${sensors[2].unit}`
+  const gpuRamPercent = `${sensors[4].value} ${sensors[4].unit}`
   const gpuRam = `${sensors[5].value} ${sensors[5].unit}`
   const gpuMemTemp = `${sensors[8].value} ${sensors[8].unit}`
   const gpuPowerPercent = `${sensors[9].value} ${sensors[9].unit}`
@@ -19,10 +41,11 @@ const ScreenV2 = () => {
   const cpuTemp = `${sensors[11].value} ${sensors[1].unit}`
   const cpuUsage = `${sensors[12].value} ${sensors[12].unit}`
   const cpuClock = `${sensors[13].value} ${sensors[13].unit}`
-  const cpuPower = `${sensors[14].value} ${sensors[14].unit}`
+  const cpuPower = `${sensors[14].value || ""} ${sensors[14].unit}`
   const ramUsedMb = `${sensors[15].value} ${sensors[15].unit}`
+  const ramUsedPercent = `${sensors[16].value} ${sensors[16].unit}`
 
-  const fps = `${sensors[16].value} ${sensors[16].unit}`
+  const fps = `${sensors[17].value} ${sensors[17].unit}`
   const waterTemp = `${sensors[19].value} ${sensors[19].unit}`
   const fanCpu = `${sensors[20].value} ${sensors[20].unit}`
   const fan1 = `${sensors[21].value} ${sensors[21].unit}`
@@ -102,56 +125,101 @@ const ScreenV2 = () => {
   ]
 
   return (
-    <Stack
-      direction={"column"}
-      justifyContent="space-between"
-      width={1}
-      p={1}
-      gap={2}
-    >
-      <Stack width={1}>
-        <Typography variant="h5">GPU</Typography>
-        {gpu.map((sensor) => (
-          <Stack
-            key={sensor.label}
-            direction="row"
-            justifyContent="space-between"
-            ml={2}
-          >
-            <Typography>{sensor.label}</Typography>
-            <Typography>{sensor.value}</Typography>
-          </Stack>
-        ))}
-      </Stack>
+    <Stack direction="row" width={1} justifyContent="space-between" gap={3}>
+      <Stack
+        direction={"column"}
+        justifyContent="space-between"
+        width={1}
+        p={1}
+        gap={2}
+      >
+        <Stack width={1}>
+          <Typography variant="h5">GPU</Typography>
+          {gpu.map((sensor) => (
+            <Stack key={sensor.label} sx={styles.containerSensorLeft}>
+              <Typography>{sensor.label}</Typography>
+              <Typography>{sensor.value}</Typography>
+            </Stack>
+          ))}
+        </Stack>
 
-      <Stack width={1}>
-        <Typography variant="h5">System</Typography>
-        {system.map((sensor) => (
-          <Stack
-            key={sensor.label}
-            direction="row"
-            justifyContent="space-between"
-            ml={2}
-          >
-            <Typography>{sensor.label}</Typography>
-            <Typography>{sensor.value}</Typography>
-          </Stack>
-        ))}
-      </Stack>
+        <Stack width={1}>
+          <Typography variant="h5">System</Typography>
+          {system.map((sensor) => (
+            <Stack key={sensor.label} sx={styles.containerSensorLeft}>
+              <Typography>{sensor.label}</Typography>
+              <Typography>{sensor.value}</Typography>
+            </Stack>
+          ))}
+        </Stack>
 
+        <Stack width={1}>
+          <Typography variant="h5">CPU</Typography>
+          {cpu.map((sensor) => (
+            <Stack key={sensor.label} sx={styles.containerSensorLeft}>
+              <Typography>{sensor.label}</Typography>
+              <Typography>{sensor.value}</Typography>
+            </Stack>
+          ))}
+        </Stack>
+      </Stack>
       <Stack width={1}>
-        <Typography variant="h5">CPU</Typography>
-        {cpu.map((sensor) => (
-          <Stack
-            key={sensor.label}
-            direction="row"
-            justifyContent="space-between"
-            ml={2}
-          >
-            <Typography>{sensor.label}</Typography>
-            <Typography>{sensor.value}</Typography>
-          </Stack>
-        ))}
+        <Typography variant="h5">Stats</Typography>
+        <Stack direction="row" sx={styles.containerSensorRight} mb={2}>
+          <Typography sx={{ marginRight: "auto" }} variant="h6">
+            FPS
+          </Typography>
+          <Typography>{fps}</Typography>
+        </Stack>
+        <Stack direction="row" sx={styles.containerSensorRight}>
+          <Typography sx={{ marginRight: "auto" }} variant="h6">
+            GPU Core
+          </Typography>
+          <Typography>{gpuTemp}</Typography>
+          <Typography>{gpuLoad}</Typography>{" "}
+          <Typography>{`${gpuPowerWatts} (${gpuPowerPercent})`}</Typography>
+        </Stack>
+
+        <Stack direction="row" sx={styles.containerSensorRight}>
+          <Typography sx={{ marginRight: "auto" }} variant="h6">
+            GPU Mem
+          </Typography>
+          <Typography>{gpuMemTemp}</Typography>
+          <Typography>{`${gpuRam} (${gpuRamPercent})`}</Typography>
+        </Stack>
+
+        <Stack direction="row" sx={styles.containerSensorRight}>
+          <Typography sx={{ marginRight: "auto" }} variant="h6">
+            CPU
+          </Typography>
+          <Typography>{cpuTemp}</Typography>
+          <Typography>{cpuUsage}</Typography>
+          <Typography>{cpuClock}</Typography>
+          <Typography>{cpuPower}</Typography>
+        </Stack>
+
+        <Stack direction="row" sx={styles.containerSensorRight}>
+          <Typography sx={{ marginRight: "auto" }} variant="h6">
+            RAM
+          </Typography>
+          <Typography>{`${ramUsedMb} (${ramUsedPercent})`}</Typography>
+        </Stack>
+
+        <Stack direction="row" sx={styles.containerSensorRight}>
+          <Typography sx={{ marginRight: "auto" }} variant="h6">
+            Water
+          </Typography>
+          <Typography>{waterTemp}</Typography>
+          <Typography>{pump}</Typography>
+        </Stack>
+
+        <Stack direction="row" sx={styles.containerSensorRight}>
+          <Typography sx={{ marginRight: "auto" }} variant="h6">
+            FAN
+          </Typography>
+          <Typography>{`${fanCpu} (CPU)`}</Typography>
+          <Typography>{`${fan4} (FRONT)`}</Typography>
+        </Stack>
       </Stack>
     </Stack>
   )
